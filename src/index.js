94924d6717b61e440e45d51fe846241ac68ae1ac
bagrounds <bagrounds@gmail.com>
2017-02-16 00:06:06 -0800

    [MINOR][ADD] makeTransition and run methods

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@
 
   /* exports */
   module.exports = funStateMachine
+  module.exports.makeTransition = makeTransition
+  module.exports.run = run
 
   /**
    *
@@ -15,12 +17,44 @@
    * @param {Object} options - all function parameters
    * @param {Function} options.next - State -> String
    * @param {Object} options.transitions - record of State -> Task State
-   * @return {Function} State -> Task
+   * @return {Function} State -> Task State
    */
   function funStateMachine (options) {
     return function (state) {
       return options.transitions[options.next(state)](state)
     }
+  }
+
+  /**
+   *
+   * @param {Object} options - all function parameters
+   * @param {Function} input - State -> Input
+   * @param {Function} action - Input -> Task Output
+   * @param {Function} update - State -> Output -> State
+   * @return {Function} State -> Task State
+   */
+  function makeTransition (options) {
+    return function (state) {
+      return options.action(options.input(state))
+        .map(options.update(state))
+    }
+  }
+
+  /**
+   *
+   * @param {Object} options - all function parameters
+   * @param {Function} machine - State -> Task State
+   * @param {Function} onError - Error -> *
+   * @param {*} state - initial state
+   */
+  function run (options) {
+    options.machine(options.state)
+      .fork(options.onError, function (newState) {
+        run({
+          machine: options.machine,
+          state: newState
+        })
+      })
   }
 })()
 
